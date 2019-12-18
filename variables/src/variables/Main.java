94319181c543513078bf31a6d319d965bd939d72
marioasensio <marioroller@gmail.com>
2019-12-18 12:26:13 +0100

    Ejercicio Variables Java

---
+++ variables/src/variables/Main.java
@@ -0,0 +1,17 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int var = 5;
+		char var2 = 'c';
+		
+		System.out.println("la variable int var contiene: " + var);
+		
+		System.out.println("la variable char var2 contiene: " + var2);
+
+	}
+
+}
